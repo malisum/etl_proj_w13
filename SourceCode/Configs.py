4eaafffbd010dda1152c937f7d2a3c8e07973d49
Sumit <malik.sum@gmail.com>
2020-02-12 02:47:07 -0500

    Sumit check in script and instructions

---
+++ SourceCode/Configs.py
@@ -0,0 +1,12 @@
+# API Key(s)
+
+# OpenWeatherMap API Key
+weather_api_key = "YOUR KEY HERE!"
+
+
+
+# Database(s)
+
+# Postgres SQL DB user & password 
+db_user = "YOUR USERID HERE"
+db_password = "YOUR PASSWORD HERE"
