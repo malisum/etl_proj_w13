4a28369bfbae9b5b0d86dca4bc0f3be4709d76ed
Sumit <malik.sum@gmail.com>
2020-02-15 11:52:29 -0500

    updated sql view

--- SqlScripts/superbowl_players_create_view.sql
+++ SqlScripts/superbowl_players_create_view.sql
@@ -1,4 +1,28 @@
 Create View superbowl_player_view As
-SELECT SB.*, PL.* from superbowl as SB
-Left Outer Join players as PL
-On (SB.mvp = PL.name)
+	SELECT 
+		SB.id, 
+		SB.superbowl, 
+		SB.superbowl_date, 
+		SB.winner, 
+		SB.loser, 
+		SB.score, 
+		SB.mvp, 
+		SB.city, 
+		SB.state, 
+		SB.stadium, 
+		SB.lat, 
+		SB.lng,
+		PL.player_id, 
+		PL.name, 
+		PL."position", 
+		PL.height, 
+		PL.height_inch, 
+		PL.weight, 
+		PL.current_team, 
+		PL.birth_date
+	FROM public.superbowl SB
+	Left Outer Join players as PL 
+		On (SB.mvp = PL.name 
+			And SB.superbowl_date > PL.birth_date
+			And PL.player_id = (Select max(player_id) from players where name = PL.name))
+	Order by SB.id
